496b60af68aa57b774c294a32b4b759f1ad3adbd
Meredith Gibbons <mgibbons1705@yahoo.co.uk>
2019-11-23 15:02:28 +0000

    Barebones menu navigation

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
+// APP
+
 var app = angular.module('myApp', []);
+
+// CONTROLLER
 app.controller('myCtrl', function($scope) {
+
     $scope.firstName= "John";
     $scope.lastName= "Doe";
+    $scope.visibilities = {
+      login : true,
+      mainMenu: false,
+      myDay: false,
+      myPeople: false,
+      myMoney: false
+    };
+
+    $scope.topLevelCategories = [1, 2, 3];
+
+    $scope.mainMenu = function() {
+      $scope.visibilities.login = false;
+      $scope.visibilities.mainMenu = true;
+    };
+
+    $scope.myDay = function(){
+      $scope.visibilities.mainMenu = false;
+      $scope.visibilities.myDay = true;
+    };
+
+    $scope.myPeople = function(){
+      $scope.visibilities.mainMenu = false;
+      $scope.visibilities.myPeople = true;
+    };
+
+    $scope.myMoney = function(){
+      $scope.visibilities.mainMenu = false;
+      $scope.visibilities.myMoney = true;
+    };
 });
