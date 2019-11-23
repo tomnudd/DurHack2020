57ea02d5273679469f5420a3dc4e90cf216c9b2b
Tom Nudd <thomas.g.nudd@durham.ac.uk>
2019-11-23 12:53:46 +0000

    uprn retrieval

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const session = require("express-session");
 const fetch = require("node-fetch");
 
+const bodyParser = require("body-parser");
+
 const passport = require("passport");
+
+const OS_KEY = "hkABo11OhSUjmTRvKi2AysevY8n2LmI7";
 
 app.use(session({
   name: "DurHack2019",
@@ -14,5 +18,17 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// RETRIEVING UPRN
+async function uprn(postcode) {
+  if (postcode && typeof(postcode) == "string") {
+    const response = await fetch("https://api.ordnancesurvey.co.uk/places/v1/addresses/postcode?postcode=" + postcode + "&key=" + OS_KEY);
+    if (response && response.ok) {
+      data = await response.json()
+      // this assumes all houses with the same postcode have the same collection date
+      return(data.results[0].DPA.UPRN);
+    }
+  }
+}
 
 module.exports = app;
