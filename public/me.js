7a3391f26b8839949465b16405be0e977fae42c8
Meredith Gibbons <mgibbons1705@yahoo.co.uk>
2019-11-23 23:36:16 +0000

    POST hobbies works

--- public/me.js
+++ public/me.js
@@ -45,18 +45,40 @@ document.addEventListener('DOMContentLoaded', function () {
     let editHobbies = document.getElementById("hobbiesEdit");
 
     editHobbies.addEventListener('click', async function () {
+        if (editHobbies.innerText != 'Edit') {
+            console.log('submitting an edit');
+
+            let text = document.getElementById("new-hobbies").value;
+            var temp = text.split("\n");
+
+            // remove blank lines
+            var dataToSend = temp.filter(function(value, index, arr){
+                return value != "";
+            });
+        
+            let response = await fetch('http://127.0.0.1:8090/hobbies/edit', {
+                method: 'POST',
+                body: JSON.stringify(dataToSend),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            // go into view mode and remake the sentences
+
+            editHobbies.innerText = 'Edit';
+
+            document.getElementById("hobbies-txt").innerHTML = "<ul>" + text + "</ul>";
+        } else {
+            // go into edit mode
+            console.log('going into edit mode');
+            editHobbies.innerText = 'Update!';
+
+            text = document.getElementById("hobbies-txt").innerText;
+            document.getElementById("hobbies-txt").innerHTML = "<textarea id=\'new-hobbies\' autofocus rows=\'5\' cols=\'45\'>" + text + "</textarea>";
+        }
+
         // either toggles to or from edit mode
-        let dataToSend = document.getElementById("hobbies-txt");
 
-        let response = await fetch('http://127.0.0.1:8090/hobbies/edit', {
-            method: 'POST',
-            body: JSON.stringify(dataToSend),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        console.log(response);
     });
 
     let favouritesEdit = document.getElementById("favouritesEdit");
@@ -77,8 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function swapHobbies() {
-    text = document.getElementById("hobbies-txt").innerText;
-    document.getElementById("hobbies-txt").innerHTML = "<textarea autofocus rows=\'5\' cols=\'45\'>" + text + "</textarea>";
+
 };
 
 function swapFavourites() {
