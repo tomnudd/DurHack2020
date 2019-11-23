5ce3c9831bad5d52813515d4c7ef2e2afdde414b
Karina <karina.talibzhanova@durham.ac.uk>
2019-11-23 15:29:01 +0000

    trying to scrape the country council page

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 const fetch = require("node-fetch");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const bodyParser = require("body-parser");
+const bodyParser = require("appbody-parser");
 
 const passport = require("passport");
 
@@ -20,15 +22,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // RETRIEVING UPRN
-async function uprn(postcode) {
-  if (postcode && typeof(postcode) == "string") {
-    const response = await fetch("https://api.ordnancesurvey.co.uk/places/v1/addresses/postcode?postcode=" + postcode + "&key=" + OS_KEY);
+async function get_uprn(address) {
+  // we can take their entire address and the OS api can process that and return json
+  if (address && typeof(address) == "string") {
+    const response = await fetch("https://api.ordnancesurvey.co.uk/places/v1/addresses/find?query=" + address + "&key=" + OS_KEY);
     if (response && response.ok) {
       data = await response.json()
-      // this assumes all houses with the same postcode have the same collection date
+      console.log(data.results[0].DPA.UPRN)
       return(data.results[0].DPA.UPRN);
     }
   }
 }
+
+// web scraping time!
+
+app.get("/test", function(req,res) {
+  return "uwu"
+});
+
+app.get("/bins/:address", function(req, res) {
+  urpn = get_urpn(req.params.address);
+  axios.get("http://mydurham.durham.gov.uk/article/12690?uprn=" + uprn) .then((response) => {
+      if(response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      let page = $("#page_PageContentHolder_template_pnlArticleBody").html();
+      console.log(page);
+  }
+  }, (error) => console.log(err) );
+});
+
 
 module.exports = app;
