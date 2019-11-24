d4f0c450492906b8f6a8114073634d9f12ed98db
Karina <karina.talibzhanova@durham.ac.uk>
2019-11-24 09:30:28 +0000

    basic calender bois

---
+++ public/day.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    document.getElementById("calendar").innerHTML = "<iframe src=\"https://calendar.google.com/calendar/embed?src=jist1h5klvj0181h8qan2gmrik%40group.calendar.google.com&ctz=Europe%2FLondon\" style=\"border: 0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\"></iframe>";
+})
+
+
+
